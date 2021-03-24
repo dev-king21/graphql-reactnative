fa558d8e78721db8c2b49972fb7b78e1b422fba9
coca-27 <cocacolla0207@gmail.com>
2021-03-25 02:39:02 +0900

    loading adding

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Button, View } from 'react-native';
+import { StyleSheet, Button, View, ActivityIndicator } from 'react-native';
 
 import ListInfo from '../components/ListInfo';
 import { getUserInfo } from '../others/getInfo';
@@ -11,6 +11,7 @@ interface Props {
 }
 
 export default function ProfileScreen(props: Props) {
+    const [isLoading, setLoading] = useState<boolean>(true)
     const [data, setData] = useState<ProfileObj>({
         avatarUrl: '',
         name: '',
@@ -27,9 +28,11 @@ export default function ProfileScreen(props: Props) {
     const userCache = async () => {
         const value: any = await AsyncStorage.getItem('OAuthToken')
         const data: any = await getUserInfo(value)
+
         try {
             if (data) {
                 setData(data)
+                setLoading(false)
             }
             // throw new Error("Error");
         } catch (e) {
@@ -41,12 +44,21 @@ export default function ProfileScreen(props: Props) {
         userCache();
     }, []);
 
+
+    const view = () => {
+        return (
+            <>
+                <ListInfo data={data} {...props} />
+                <View style={styles.logout}><Button onPress={() => { AsyncStorage.removeItem('OAuthToken').then(() => { props.navigation.push('Login') }) }} title="LOGOUT" /></View>
+            </>
+        );
+    }
+
     return (
         <View style={styles.container}>
-            <ListInfo data={data} {...props} />
-            <View style={styles.logout}><Button onPress={() => { AsyncStorage.removeItem('OAuthToken').then(() => { props.navigation.push('Login') }) }} title="LOGOUT" /></View>
+            {isLoading ? <View style={styles.loading}><ActivityIndicator size="large" /></View> : view()}
         </View>
-    );
+    )
 }
 
 const styles = StyleSheet.create({
@@ -57,5 +69,11 @@ const styles = StyleSheet.create({
     logout: {
         width: '80%',
         borderRadius: 5
+    },
+    loading: {
+        display: 'flex',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100%'
     }
 });
--- screens/RepositoryScreen.tsx
+++ screens/RepositoryScreen.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, ScrollView, View, Text } from 'react-native';
+import { StyleSheet, ScrollView, View, Text, ActivityIndicator } from 'react-native';
 import { getRepo } from '../others/getInfo';
 import { RepositoryObj } from '../components/types';
 import AsyncStorage from '@react-native-community/async-storage';
 
 export default function RepositoryScreen() {
+    const [isLoading, setLoading] = useState<boolean>(true)
     const [lists, setLists] = useState<RepositoryObj[]>([]);
 
     useEffect(() => {
@@ -21,28 +22,35 @@ export default function RepositoryScreen() {
                     repositoryList.push(item)
                 })
                 setLists(repositoryList)
+                setLoading(false)
             }
         } catch (err) {
             console.log(err)
         }
     }
 
-    return (
-        <ScrollView>
-            {
-                lists.map((item: any, index) =>
-                    <View key={index} style={styles.container}>
-                        <View style={{ flex: 1 }}>
-                            <Text style={styles.title}>{item.name}</Text>
-                            <Text>{item.description}</Text>
+    const view = () => {
+        return (
+            <ScrollView>
+                {
+                    lists.map((item: any, index) =>
+                        <View key={index} style={styles.container}>
+                            <View style={{ flex: 1 }}>
+                                <Text style={styles.title}>{item.name}</Text>
+                                <Text>{item.description}</Text>
+                            </View>
+                            <Text>
+                                {item.owner.login}
+                            </Text>
                         </View>
-                        <Text>
-                            {item.owner.login}
-                        </Text>
-                    </View>
-                )
-            }
-        </ScrollView>
+                    )
+                }
+            </ScrollView>
+        )
+    }
+    return (<>
+        {isLoading ? <View style={styles.loading}><ActivityIndicator size="large" /></View> : view()}
+    </>
     );
 }
 
@@ -61,4 +69,10 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontWeight: 'bold',
     },
+    loading: {
+        display: 'flex',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100%'
+    }
 });
